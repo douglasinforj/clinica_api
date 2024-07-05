e29c23ac8fcd4311b904787a997a29b4d5b7864c
Douglas Silva <douglasitpro@gmail.com>
2024-07-05 00:20:04 -0300

    Configurando rotas de autenticação

--- clinica/urls.py
+++ clinica/urls.py
@@ -52,6 +52,13 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+#rota de autenticação:
+
+from rest_framework_simplejwt import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 
 # rotas urls
@@ -61,6 +68,11 @@ urlpatterns = [
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    #rotas de autenticação
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     
 ]
 if settings.DEBUG:
