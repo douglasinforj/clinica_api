ca13d0540a505c20312a0b6f2a0b8ed14a15a1f9
Douglas Silva <douglasitpro@gmail.com>
2024-07-05 00:22:11 -0300

    Protegendo as Rotas com permission_classes

--- clinica_app/api/viewsets.py
+++ clinica_app/api/viewsets.py
@@ -2,15 +2,23 @@ from rest_framework import viewsets
 from clinica_app.models import Cliente, Exame, ExameMarcado
 from .serializers import ClienteSerializer, ExameSerializer, ExameMarcadoSerializer
 
+from rest_framework.permissions import IsAuthenticated
+
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
+
+    permission_classes = [IsAuthenticated]
 
 
 class ExameViewSet(viewsets.ModelViewSet):
     queryset = Exame.objects.all()
     serializer_class = ExameSerializer
 
+    permission_classes = [IsAuthenticated]
+
 class ExameMarcadoViewSet(viewsets.ModelViewSet):
     queryset = ExameMarcado.objects.all()
     serializer_class = ExameMarcadoSerializer
+
+    permission_classes = [IsAuthenticated]
