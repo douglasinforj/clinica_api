d839ac1398ce56adaca1b8ec6b220f2cc657bee1
Douglas Silva <douglasitpro@gmail.com>
2024-07-04 23:04:41 -0300

    modelando Cliente

--- clinica_app/models.py
+++ clinica_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Cliente(models.Model):
+    foto = models.ImageField(upload_to='foto_clientes/', blank=True, null=True)
+    nome = models.CharField(max_length=255)
+    rg = models.CharField(max_length=12, unique=True)
+    cpf = models.CharField(max_length=14, unique=True)
+    telefone = models.CharField(max_length=20, blank=True, null=True)
+    email = models.CharField(max_length=254, blank=True, null=True)
+    data_nascimento = models.DateField()
+    data_cadastro = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+    
