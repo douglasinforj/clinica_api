6ef1f113204d5104e721ab4668b544e788be7406
Douglas Silva <douglasitpro@gmail.com>
2024-07-08 20:43:38 -0300

    Ajustando ExameMarcadoViewSet para marcações de exames para cliente

--- clinica_app/api/viewsets.py
+++ clinica_app/api/viewsets.py
@@ -1,8 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from clinica_app.models import Cliente, Exame, ExameMarcado
 from .serializers import ClienteSerializer, ExameSerializer, ExameMarcadoSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
+from rest_framework.response import Response
 
 
 class ClienteViewSet(viewsets.ModelViewSet):
@@ -11,7 +12,7 @@ class ClienteViewSet(viewsets.ModelViewSet):
 
     permission_classes = [IsAuthenticated]
 
-    parser_classes = [MultiPartParser, FormParser, JSONParser]         #preparando a classe para receber multipart/form-data, envio de foto
+    parser_classes = [MultiPartParser, FormParser, JSONParser]         
 
     #garantindo que aceite  metodo Patch  com dados de formulários
     def partial_update(self, request, *args, **kwargs):
@@ -25,11 +26,24 @@ class ExameViewSet(viewsets.ModelViewSet):
 
     permission_classes = [IsAuthenticated]
 
+    parser_classes = [MultiPartParser, FormParser, JSONParser]  
+
+    
+
+   
+
 class ExameMarcadoViewSet(viewsets.ModelViewSet):
     queryset = ExameMarcado.objects.all()
     serializer_class = ExameMarcadoSerializer
-
     permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser, JSONParser]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 
