cccb1c8984a235462e002caba17bf4446c8805da
Douglas Silva <douglasitpro@gmail.com>
2024-07-08 20:46:28 -0300

    Ajustando a classe ExameMarcado para validar documentos enviados

--- clinica_app/api/serializers.py
+++ clinica_app/api/serializers.py
@@ -15,3 +15,11 @@ class ExameMarcadoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExameMarcado
         fields = '__all__'
+
+    def create(self, validated_data):
+        documentos_exames = validated_data.pop('documentos_exames', None)
+        exame_marcado = ExameMarcado.objects.create(**validated_data)
+        if documentos_exames:
+            exame_marcado.documentos_exames = documentos_exames
+            exame_marcado.save()
+        return exame_marcado
